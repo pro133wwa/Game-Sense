7f763c1ef1f3217a2d723c125618234157b9a662
Dig <shop.felx@mail.ru>
2022-12-18 23:48:16 +0300

    DiGGeR Plash!

--- Game/Sense/client/feature/impl/combat/AutoPotion.java
+++ Game/Sense/client/feature/impl/combat/AutoPotion.java
@@ -7,10 +7,13 @@ import Game.Sense.client.feature.impl.FeatureCategory;
 import Game.Sense.client.ui.settings.Setting;
 import Game.Sense.client.ui.settings.impl.BooleanSetting;
 import Game.Sense.client.ui.settings.impl.NumberSetting;
+import Game.Sense.client.utils.GuiBossOverlay;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.ClickType;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.CPacketHeldItemChange;
@@ -22,93 +25,156 @@ import net.minecraft.potion.PotionUtils;
 import net.minecraft.util.EnumHand;
 
 public class AutoPotion extends Feature {
+
     public NumberSetting delay;
-
-    public BooleanSetting onlyGround = new BooleanSetting("Only Ground", true, () -> Boolean.valueOf(true));
-
-    public BooleanSetting strenght = new BooleanSetting("Strenght", true, () -> Boolean.valueOf(true));
-
-    public BooleanSetting speed = new BooleanSetting("Speed", true, () -> Boolean.valueOf(true));
-
-    public BooleanSetting fire_resistance = new BooleanSetting("Fire Resistance", true, () -> Boolean.valueOf(true));
-
-    private BooleanSetting hotberOnly = new BooleanSetting("Hotbar Only", true, () -> Boolean.valueOf(true));
-
-    ItemStack held;
+    public BooleanSetting onlyGround = new BooleanSetting("Only Ground", true, () -> true);
+    public BooleanSetting onlyKT = new BooleanSetting("KT Only", true, () -> true);
 
     public AutoPotion() {
-        super("AutoPotion", "\u0410\u0432\u0442\u043e\u043c\u0430\u0442\u0438\u0447\u0435\u0441\u043a\u0438 \u0431\u0440\u043e\u0441\u0430\u0435\u0442 Splash \u0437\u0435\u043b\u044c\u044f \u043d\u0430\u0445\u043e\u0434\u044f\u0449\u0438\u0435\u0441\u044f \u0432 \u0438\u043d\u0432\u0435\u043d\u0442\u0430\u0440\u0435", FeatureCategory.Combat);
-        this.delay = new NumberSetting("Throw Delay", 300.0F, 10.0F, 800.0F, 10.0F, () -> Boolean.valueOf(true));
-        addSettings(new Setting[] { (Setting)this.delay, (Setting)this.hotberOnly, (Setting)this.onlyGround, (Setting)this.strenght, (Setting)this.speed, (Setting)this.fire_resistance });
+        super("AutoPotion", "????????????? ??????? ????? ??????????? ? ???????(??????? ?????)",  FeatureCategory.Combat);
+        this.delay = new NumberSetting("Delay", 500, 0, 600, 10, () -> true);
+        addSettings(delay, onlyGround, onlyKT);
     }
 
     @EventTarget
-    public void onPre(EventPreMotion event) {
-        if (this.timerHelper.hasReached(this.delay.getNumberValue())) {
-            if (this.strenght.getBoolValue() &&
-                    !mc.player.isPotionActive(MobEffects.STRENGTH))
-                throwPotion(5);
-            if (this.speed.getBoolValue() &&
-                    !mc.player.isPotionActive(MobEffects.SPEED))
-                throwPotion(1);
-            if (this.fire_resistance.getBoolValue() &&
-                    !mc.player.isPotionActive(MobEffects.FIRE_RESISTANCE))
-                throwPotion(12);
-            this.timerHelper.reset();
+    public void onUpdate(EventPreMotion event) {
+        if ((mc.currentScreen instanceof GuiInventory)) {
+            return;
+        }
+        if (onlyGround.getBoolValue() && !mc.player.onGround) {
+            return;
+        }
+        if (KillAura.timerHelper.hasReached(delay.getNumberValue())) {
+            if (!mc.player.isPotionActive((Potion.getPotionById(1))) && isPotionOnHotBar(Potions.SPEED)) {
+                if (onlyKT.getBoolValue() && !GuiBossOverlay.pot) {
+                    return;
+                }
+                if (mc.player.onGround) {
+                    event.setPitch(90);
+                } else {
+                    event.setPitch(-90);
+
+                }
+                if (mc.player.onGround) {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Rotation(mc.player.rotationYaw, 90, mc.player.onGround));
+                } else {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Rotation(mc.player.rotationYaw, -90, mc.player.onGround));
+                }
+                if (mc.player.onGround) {
+                    if (event.getPitch() == 90) {
+                        throwPot(Potions.SPEED);
+                    }
+                } else {
+                    if (event.getPitch() == -90) {
+                        throwPot(Potions.SPEED);
+                    }
+                }
+            }
+            if (!mc.player.isPotionActive((Potion.getPotionById(5))) && isPotionOnHotBar(Potions.STRENGTH)) {
+                if (mc.player.onGround) {
+                    event.setPitch(90);
+                } else {
+                    event.setPitch(-90);
+                }
+                if (mc.player.onGround) {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Rotation(mc.player.rotationYaw, 90, mc.player.onGround));
+                } else {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Rotation(mc.player.rotationYaw, -90, mc.player.onGround));
+                }
+                if (mc.player.onGround) {
+                    if (event.getPitch() == 90) {
+                        throwPot(Potions.STRENGTH);
+                    }
+                } else {
+                    if (event.getPitch() == -90) {
+                        throwPot(Potions.STRENGTH);
+                    }
+                }
+            }
+            if (!mc.player.isPotionActive((Potion.getPotionById(12))) && isPotionOnHotBar(Potions.FIRERES)) {
+                if (mc.player.onGround) {
+                    event.setPitch(90);
+                } else {
+                    event.setPitch(-90);
+                }
+                if (mc.player.onGround) {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Rotation(mc.player.rotationYaw, 90, mc.player.onGround));
+                } else {
+                    mc.player.connection.sendPacket(new CPacketPlayer.Rotation(mc.player.rotationYaw, -90, mc.player.onGround));
+                }
+                if (mc.player.onGround) {
+                    if (event.getPitch() == 90) {
+                        throwPot(Potions.FIRERES);
+                    }
+                } else {
+                    if (event.getPitch() == -90) {
+                        throwPot(Potions.FIRERES);
+                    }
+                }
+            }
+            KillAura.timerHelper.reset();
         }
     }
 
-    private int getPotionIndexInv(int id) {
-        for (int i = 0; i < 45; i++) {
-            int index = (i < 9) ? (i + 36) : i;
-            for (PotionEffect potion : PotionUtils.getEffectsFromStack(mc.player.inventory.getStackInSlot(index))) {
-                if (potion.getPotion() == Potion.getPotionById(id) && mc.player.inventory.getStackInSlot(i).getItem() == Items.SPLASH_POTION)
-                    return index;
-            }
+    void throwPot(Potions potion) {
+        int slot = getPotionSlot(potion);
+        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+        mc.playerController.updateController();
+        mc.player.connection.sendPacket(new CPacketPlayerTryUseItem(EnumHand.MAIN_HAND));
+        mc.playerController.updateController();
+        mc.player.connection.sendPacket(new CPacketHeldItemChange(mc.player.inventory.currentItem));
+    }
+
+    public int getPotionSlot(Potions potion) {
+        for (int i = 0; i < 9; ++i) {
+            if (this.isStackPotion(mc.player.inventory.getStackInSlot(i), potion))
+                return i;
         }
         return -1;
     }
 
-    private int getPotionIndexHb(int id) {
-        for (int i = 0; i < 9; i++) {
-            for (PotionEffect potion : PotionUtils.getEffectsFromStack(mc.player.inventory.getStackInSlot(i))) {
-                if (potion.getPotion() == Potion.getPotionById(id) && mc.player.inventory.getStackInSlot(i).getItem() == Items.SPLASH_POTION)
-                    return i;
+    public boolean isPotionOnHotBar(Potions potions) {
+        for (int i = 0; i < 9; ++i) {
+            if (isStackPotion(mc.player.inventory.getStackInSlot(i), potions)) {
+                return true;
             }
         }
-        return -1;
+        return false;
     }
 
-    private void throwPotion(int potionId) {
-        if (this.onlyGround.getBoolValue() && !mc.player.onGround)
-            return;
-        int index = -1;
-        if (getPotionIndexHb(potionId) == -1) {
-            index = getPotionIndexInv(potionId);
-        } else {
-            index = getPotionIndexHb(potionId);
-        }
-        if (index == -1)
-            return;
-        if (!this.hotberOnly.getBoolValue() && index > 9) {
-            mc.playerController.windowClick(0, index, 0, ClickType.PICKUP, (EntityPlayer)mc.player);
-            mc.playerController.windowClick(0, 44, 0, ClickType.PICKUP, (EntityPlayer)mc.player);
-            throwPot(index);
-            mc.playerController.windowClick(0, 44, 0, ClickType.PICKUP, (EntityPlayer)mc.player);
-        } else {
-            throwPot(index);
-        }
-    }
+    public boolean isStackPotion(ItemStack stack, Potions potion) {
+        if (stack == null)
+            return false;
 
-    void throwPot(int index) {
-        mc.player.connection.sendPacket((Packet)new CPacketHeldItemChange((index < 9) ? index : 8));
-        mc.player.connection.sendPacket((Packet)new CPacketPlayer.Rotation(mc.player.rotationYaw, 90.0F, mc.player.onGround));
-        mc.player.rotationPitchHead = 90.0F;
-        mc.player.connection.sendPacket((Packet)new CPacketPlayerTryUseItem(EnumHand.MAIN_HAND));
-        mc.player.connection.sendPacket((Packet)new CPacketHeldItemChange(mc.player.inventory.currentItem));
+        Item item = stack.getItem();
+
+        if (item == Items.SPLASH_POTION) {
+            int id = 5;
+
+            switch (potion) {
+                case STRENGTH: {
+                    break;
+                }
+                case SPEED: {
+                    id = 1;
+                    break;
+                }
+                case FIRERES: {
+                    id = 12;
+                    break;
+                }
+            }
+
+            for (PotionEffect effect : PotionUtils.getEffectsFromStack(stack)) {
+                if (effect.getPotion() == Potion.getPotionById(id)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     enum Potions {
-        STRENGTH, SPEED, FIRERES;
+        STRENGTH, SPEED, FIRERES
     }
 }
