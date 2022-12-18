0dfebfa712d367da48d5d45acdd2a38cb99fded6
Dig <shop.felx@mail.ru>
2022-12-18 23:34:08 +0300

    DiGGeR Plash!

--- Game/Sense/client/feature/impl/hud/FeatureList.java
+++ Game/Sense/client/feature/impl/hud/FeatureList.java
@@ -64,9 +64,9 @@ public class FeatureList extends Feature {
                 if (feature.animYto > 0.01f) {
                     if (feature.getSuffix().equals("ClickGui") || noVisualModules.getBoolValue() && feature.getCategory() == FeatureCategory.Visuals || onlyBinds.getBoolValue() && feature.getBind() == 0)
                         continue;
-                    stringWidth = this.mc.rubik_18.getStringWidth(feature.getLabel().toLowerCase()) + 3;
-                    RenderUtils.drawRect4(displayWidth + 50 - Helper.mc.rubik_18.getStringWidth(feature.getLabel().toLowerCase()) - 5, y, displayWidth + 50, y + (float)offset + 8.2f,RenderUtils.injectAlpha(ClientHelper.getClientColor(y, yTotal, 25), 255).getRGB());
-                    Helper.mc.rubik_18.drawString(feature.getLabel().toLowerCase(), displayWidth + 50.5f - Helper.mc.rubik_15.getStringWidth(feature.getLabel().toLowerCase()) - 4f, y + Helper.mc.rubik_15.getFontHeight() + (float)offset - 4, -1);
+                    stringWidth = this.mc.rubik_15.getStringWidth(feature.getLabel().toLowerCase()) + 3;
+                    RenderUtils.drawRect4(displayWidth + 50 - Helper.mc.rubik_15.getStringWidth(feature.getLabel().toLowerCase()) - 5, y, displayWidth + 50, y + (float)offset + 8.2f,RenderUtils.injectAlpha(ClientHelper.getClientColor(y, yTotal, 25), 255).getRGB());
+                    Helper.mc.rubik_15.drawString(feature.getLabel().toLowerCase(), displayWidth + 50.5f - Helper.mc.rubik_15.getStringWidth(feature.getLabel().toLowerCase()) - 4f, y + Helper.mc.rubik_15.getFontHeight() + (float)offset - 4, -1);
                     RenderUtils.drawRect4(displayWidth + 49, y, displayWidth +51.5f, y + 8.2f + (float)offset, Color.WHITE.getRGB());
 
                     y += 8 * feature.animYto;
@@ -84,13 +84,13 @@ public class FeatureList extends Feature {
                 if (module.animYto > 0.01f) {
                     if (module.getSuffix().equals("ClickGui") || noVisualModules.getBoolValue() && module.getCategory() == FeatureCategory.Visuals || onlyBinds.getBoolValue() && module.getBind() == 0)
                         continue;
-                    stringWidth = this.mc.rubik_18.getStringWidth(module.getLabel().toLowerCase()) + 3;
+                    stringWidth = this.mc.rubik_15.getStringWidth(module.getLabel().toLowerCase()) + 3;
                     GlStateManager.pushMatrix();
                     RenderUtils.drawBlurredShadow(displayWidth -2, y + (float)offset - 3.5f + 2, stringWidth + 5f, 10, 2, RenderUtils.injectAlpha(ClientHelper.getClientColor(y, yTotal, 10), 250));
                     GL11.glTranslated(1, y, 1);
                     GL11.glTranslated(-1, -y, 1);
                     RenderUtils.drawRect(displayWidth, y - 0.5f -2 + offset + 2, displayWidth + (float)stringWidth + 3.5f, y + (float)offset + 8.0f, RenderUtils.injectAlpha(ClientHelper.getClientColor(y, yTotal, 25), (int) (animYto * 255)).getRGB());
-                    this.mc.rubik_18.drawString(module.getLabel().toLowerCase(), displayWidth + 3.5f, y + (float)offset + 2, Color.WHITE.getRGB());
+                    this.mc.rubik_15.drawString(module.getLabel().toLowerCase(), displayWidth + 3.5f, y + (float)offset + 2, Color.WHITE.getRGB());
                     RenderUtils.drawRect(displayWidth - 1.5f, y - 0.5f -2 + offset + 2, displayWidth + 1 , y + (float)offset +8f,  Color.WHITE.getRGB());
                     GlStateManager.popMatrix();
 
