61ff62019843bc3a6a2ecac2d93b4f9f03080195
777 <haskadoger@gmail.com>
2022-12-19 16:52:30 +0200

    shadermainmenu

--- Game/Sense/client/module/ModuleManager.java
+++ Game/Sense/client/module/ModuleManager.java
@@ -1,6 +1,7 @@
 package Game.Sense.client.module;
 
 import Game.Sense.client.module.feature.AAA.DeathCoordinates;
+import Game.Sense.client.module.feature.AAA.WaterDisabler;
 import Game.Sense.client.module.feature.ModuleCategory;
 import Game.Sense.client.module.feature.COMBAT.*;
 import Game.Sense.client.module.feature.MOVEMENT.*;
@@ -83,6 +84,7 @@ public class ModuleManager {
         features.add(new Jesus());
         features.add(new Strafe());
         features.add(new Timer());
+        features.add(new WaterDisabler());
         //features.add(new NoWeb());
         //features.add(new ElytraFly());
 
