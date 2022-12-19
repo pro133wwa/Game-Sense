4b77c9911e7189909a39607cd7a03eb4fe56bbf0
UwU_#5276 <110829847+pro133wwa@users.noreply.github.com>
2022-12-19 12:07:47 +0300

    UwU_#5276 null

--- Game/Sense/client/Coder/UwU_/UwU_.java
+++ Game/Sense/client/Coder/UwU_/UwU_.java
@@ -4,17 +4,17 @@ public class UwU_ {
         /*
     что добавили и где
     ------------------
-    1)UID.java, GameSense(UID)
+    1) UID.java, GameSense(UID)
 
-    2)EntityList FULL FIX
+    2) EntityList FULL FIX
 
-    3)PaletteHelper, TimerSmart
+    3) PaletteHelper, TimerSmart
 
     4) new Baza
 
     5) new Baza2
 
-    6)Button
+    6) Button
 
     7)
 
