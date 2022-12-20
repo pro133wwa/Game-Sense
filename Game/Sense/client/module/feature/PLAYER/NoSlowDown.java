b4c53e53152b4dd7af4ea26e8010faea83ff85bd
UwU_#5276 <110829847+pro133wwa@users.noreply.github.com>
2022-12-20 14:03:14 +0300

    UwU_#5276 NoSlowDown

--- Game/Sense/client/module/feature/PLAYER/NoSlowDown.java
+++ Game/Sense/client/module/feature/PLAYER/NoSlowDown.java
@@ -1,6 +1,7 @@
 package Game.Sense.client.module.feature.PLAYER;
 
 import Game.Sense.client.Helper.EventTarget;
+import Game.Sense.client.Helper.Utility.Helper;
 import Game.Sense.client.Helper.events.impl.packet.EventSendPacket;
 import Game.Sense.client.Helper.events.impl.player.EventUpdate;
 import Game.Sense.client.module.Module;
@@ -12,44 +13,35 @@ import net.minecraft.network.play.client.CPacketPlayer;
 
 public class NoSlowDown extends Module {
     public static NumberSetting percentage = new NumberSetting("Percentage", 100, 0, 100, 1, () -> true, NumberSetting.NumberType.PERCENTAGE);
-    private final ListSetting noSlowMode = new ListSetting("NoSlow Mode", "Matrix", () -> true, "Vanilla", "Matrix", "Matrix New");
+    private final ListSetting noSlowMode = new ListSetting("NoSlow Mode", "Matrix", () -> true, "Vanilla", "Matrix");
     public int usingTicks;
 
     public NoSlowDown() {
-        super("NoSlowDown", "Убирает замедление при использовании еды и других предметов", ModuleCategory.PLAYER);
+        super("NoSlowDown", "Позволяет есть на ходу", ModuleCategory.PLAYER);
         addSettings(noSlowMode, percentage);
     }
 
     @EventTarget
     public void onSendPacket(EventSendPacket eventSendPacket) {
-        this.setSuffix(noSlowMode.getCurrentMode() + ", " + percentage.getNumberValue() + "%");
         CPacketPlayer packet = (CPacketPlayer) eventSendPacket.getPacket();
-        if (noSlowMode.currentMode.equals("Matrix New")) {
-            if (mc.player.isHandActive() && MovementUtils.isMoving() && !mc.gameSettings.keyBindJump.pressed) {
-                packet.y = mc.player.ticksExisted % 2 == 0 ? packet.y + 0.0006 : packet.y + 0.0002;
-                packet.onGround = false;
-                mc.player.onGround = false;
-            }
-        }
     }
 
     @EventTarget
     public void onUpdate(EventUpdate event) {
-        this.setSuffix(noSlowMode.getCurrentMode() + ", " + percentage.getNumberValue() + "%");
-        usingTicks = mc.player.isUsingItem() ? ++usingTicks : 0;
-        if (!this.isEnabled() || !mc.player.isUsingItem()) {
+        usingTicks = Helper.mc.player.isUsingItem() ? ++usingTicks : 0;
+        if (!this.isEnabled() || !Helper.mc.player.isUsingItem()) {
             return;
         }
         if (noSlowMode.currentMode.equals("Matrix")) {
-            if (mc.player.isUsingItem()) {
-                if (mc.player.onGround && !mc.gameSettings.keyBindJump.isKeyDown()) {
-                    if (mc.player.ticksExisted % 2 == 0) {
-                        mc.player.motionX *= 0.35;
-                        mc.player.motionZ *= 0.35;
+            if (Helper.mc.player.isUsingItem()) {
+                if (Helper.mc.player.onGround && !Helper.mc.gameSettings.keyBindJump.isKeyDown()) {
+                    if (Helper.mc.player.ticksExisted % 2 == 0) {
+                        Helper.mc.player.motionX *= 0.35;
+                        Helper.mc.player.motionZ *= 0.35;
                     }
-                } else if (mc.player.fallDistance > 0.2) {
-                    mc.player.motionX *= 0.9100000262260437;
-                    mc.player.motionZ *= 0.9100000262260437;
+                } else if (Helper.mc.player.fallDistance > 0.2) {
+                    Helper.mc.player.motionX *= 0.9100000262260437;
+                    Helper.mc.player.motionZ *= 0.9100000262260437;
                 }
             }
         }
