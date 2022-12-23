3cdcc1787eefd86d9bdab0f809e1b19bcac308eb
DiGGeRNAUD <shop.felx@mail.ru>
2022-12-24 00:04:49 +0300

    DiscordHelper DiGGeR

--- Game/Sense/client/Helper/Utility/other/DiscordHelper.java
+++ Game/Sense/client/Helper/Utility/other/DiscordHelper.java
@@ -17,10 +17,10 @@ public class DiscordHelper implements Helper {
             DiscordRPC.discordInitialize(discordID, eventHandlers, true, null);
             DiscordHelper.discordRichPresence.startTimestamp = System.currentTimeMillis() / 1000L;
             DiscordHelper.discordRichPresence.details = "Role: " + "Delvoper";
-            DiscordHelper.discordRichPresence.largeImageKey = "https://steamuserimages-a.akamaihd.net/ugc/1699529224113152957/5C738FA2908C3446C9BFB31835A0A83D31AA2D20/?imw=512&amp;&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=false";
+            DiscordHelper.discordRichPresence.largeImageKey = "https://media.discordapp.net/attachments/1055376209162276945/1055953913565679646/standard_2.gif";
             DiscordHelper.discordRichPresence.largeImageText = "vk.com/HUNNER.cc";
             DiscordHelper.discordRichPresence.smallImageText = "UID: " + "0 | Name: " + "DiGGeR";
-            DiscordHelper.discordRichPresence.smallImageKey = "https://steamuserimages-a.akamaihd.net/ugc/1699529224113152957/5C738FA2908C3446C9BFB31835A0A83D31AA2D20/?imw=512&amp;&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=false";
+            DiscordHelper.discordRichPresence.smallImageKey = "https://steamuserimages-a.akamaihd.net/ugc/1821118493985004024/0F9DD7AA19E5EB4BD19338050EC0E6C0407D87B4/?imw=512&amp;imh=284&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true";
             DiscordHelper.discordRichPresence.state = "Version: " + GameSense.instance.version;
             DiscordRPC.discordUpdatePresence(discordRichPresence);
         } catch (Exception e) {
